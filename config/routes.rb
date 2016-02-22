375486df04eb2eb50c11e530f59a5b529fdf4aba
hieunm6 <hieunmce@gmail.com>
2016-02-23 00:10:56 +0700

    create login session with current user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :users
+  get 'sessions/new'
   get 'signup' => "users#new"
   root "wellcome#index"
-
+  get 'login' => "sessions#new"
+  post 'login' => "sessions#create"
+  get 'logout' => "sessions#destroy"
 end
