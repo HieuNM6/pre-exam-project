d8561faa8b52f34de46aec3c8464978c9f3ee708
hieunm6 <hieunmce@gmail.com>
2016-02-23 20:51:14 +0700

    Create logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+      log_in @user
 			flash[:success] = "Wellcome to the sharing stories app!"
 			redirect_to @user
 		else
